af29e5a1af5b6ea3f0e53bdc7cddbf500c8c30b7
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-11-07 11:27:15 +0900

    [feat] 24479.cpp done

--- code/boj/24479.cpp
+++ code/boj/24479.cpp
@@ -1,61 +1,46 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+int N,M,R;
+int cnt=1;
 vector<vector<int>> maps;
 vector<int> visit;
 vector<int> ans;
-int order = 0;
 
-void DFS(int cur){
-  if(visit[cur]) return;
-  visit[cur] = 1;
-  ans[cur]=++order;
-  vector<int> &connected = maps[cur];
-  sort(connected.begin(), connected.end());
-
-  // cout<< cur<<"에 연결된 점";
-  // for(int i=0;i<connected.size();i++){
-  //   cout << connected[i] << " ";
-  // };
-  // cout << "\n";
-
-  for(int i=0; i<connected.size();i++){
-    if(visit[connected[i]]) continue;
-    // cout << cur << "에서"<<connected[i] << "으로 이동" << "\n";
-    DFS(connected[i]);
+void DFS(int v){
+  ans[v] = cnt++;
+  for(int i=0;i<maps[v].size();i++){
+    int cur = maps[v][i];
+    if(visit[cur]) continue;
+    visit[cur] = 1;
+    DFS(cur);
   }
 }
 
-int main(){
-  int n,m,r,u,v;
-  cin >> n >> m >> r;
-
-  maps.resize(n+1, vector<int>());
-  visit.resize(n+1, 0);
-  ans.resize(n+1,0);
-  for(int i=0;i<m;i++){
-    cin >> u >> v;
-    maps[u].push_back(v);
-    maps[v].push_back(u);
-  };
-  
-  DFS(r);
-
-  // for(int i=1;i<n;i++){
-  //   vector<int> temp = maps[i];
-  //   cout << i <<"번째" ;
-  //   for(int j=0;j<temp.size();j++){
-  //     cout << temp[j] << " ";
-  //   }
-  //   cout << "\n";
-  // }
-
-  for(int i=1;i<ans.size();i++){
+int main(void){
+  ios::sync_with_stdio(0);
+  cout.tie(0);
+  cin >> N >> M >> R;
+  maps.resize(N+1, vector<int>());
+  visit.resize(N+1, 0);
+  ans.resize(N+1, 0);
+  for(int i=0;i<M;i++){
+    int start,end;
+    cin >> start >> end;
+    maps[start].push_back(end);
+    maps[end].push_back(start);
+  }
+  for(int i=0;i<maps.size();i++){
+    sort(maps[i].begin(), maps[i].end());
+    // for(int j=0;j<maps[i].size();j++){
+    //   cout << maps[i][j] << " ";
+    // }
+    // cout << "\n";
+  }
+  visit[R] = cnt;
+  DFS(R);
+  for(int i=1;i<ans.size();i++)
     cout << ans[i] << "\n";
-  };
-  
-  return 0;
-};
+}
