83314909a2bde9f4bc503296e98cde3158fac7d7
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-12-04 18:07:14 +0900

    [chore] 1992.cpp refactor

--- code/boj/1992.cpp
+++ code/boj/1992.cpp
@@ -4,28 +4,18 @@ using namespace std;
 
 int N;
 string str;
-vector<vector<string>> maps;
+vector<vector<int>> maps;
 
 void input(){
   cin >> N;
-  maps.resize(N);
+  maps.resize(N, vector<int>(N));
   for(int i=0;i<N;i++){
     cin >> str;
-    vector<string> row;
-    row.resize(N);
-    for(int j=0;j<str.size();j++) row[j] = str[j];
-    maps[i] = row;
+    for(int j=0;j<str.size();j++) maps[i][j] = str[j] - '0';
   }
-  // for(int i=0;i<maps.size();i++){
-  //   for(int j=0;j<maps[i].size();j++){
-  //     cout << maps[i][j] << " ";
-  //   }
-  //   cout << "\n";
-  // }
 }
 
 void operation(int x, int y, int n){
-
   // check same or differnt 
   bool same = true;
   for(int i=x;i<n+x;i++){
