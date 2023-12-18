a9d26a8bc7afe9a9eb334a98b423f780f6fdb76a
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-12-18 12:03:44 +0900

    [refactor] optimize memory

--- lecture/inflearn_js/8.recur.dfs/subset.js
+++ lecture/inflearn_js/8.recur.dfs/subset.js
@@ -2,7 +2,7 @@ const target = 3;
 const arr = Array.from({ length: target + 1 }, (_, i) => i);
 const dp = Array.from({ length: target + 1 }, () => false);
 
-function DFS(n, dp) {
+function DFS(n) {
   if (n > target) {
     let ans = "";
     for (let i = 1; i < dp.length; i++) if (dp[i]) ans += `${i} `;
@@ -10,10 +10,10 @@ function DFS(n, dp) {
     return;
   }
 
-  const use = [...dp];
-  use[n] = true;
-  DFS(n + 1, use);
-  DFS(n + 1, dp);
+  dp[n] = true;
+  DFS(n + 1);
+  dp[n] = false;
+  DFS(n + 1);
 }
 
 DFS(1, dp);
