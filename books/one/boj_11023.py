b68c2e84817352574502e9e31eb294a306f22272
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-12-18 20:50:20 +0900

    [refactor] boj_11023.py

--- books/one/boj_11023.py
+++ books/one/boj_11023.py
@@ -1,27 +1,9 @@
-import sys
-input = sys.stdin.readline
-a,b = map(int, input().split())
-def sol():
-  
-  cnt = 1
-  val = 1
-  idx = 0
-  ans = 0
-  arr = []
-  while True:
-    if idx >= b:
-      break
-    if cnt==val:
-      for _ in range(0,val):
-        arr.append(val)
-        idx+=1
-      cnt = 1
-      val+=1
-    else:
-      cnt+=1
-  
-  for idx,v in enumerate(arr):
-    if idx>=a-1 and b-1>=idx:
-      ans += v
-  print(ans)
-sol()
+a,b = map(int,input().split())
+l = []
+for i in range(1,50):
+    for j in range(i):
+        l.append(i)
+s = 0
+for i in range(a-1,b):
+    s+=l[i]
+print(s)
